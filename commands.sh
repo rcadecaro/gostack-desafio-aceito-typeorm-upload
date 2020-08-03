1940145ef2247b8f215e806a52d0ed63212854e3
Renato Cadecaro <rcadecaro@gmail.com>
2020-08-02 21:52:48 -0300

    chore: Transactions

---
+++ commands.sh
@@ -0,0 +1,3 @@
+yarn typeorm migration:create -n CreateCategories
+yarn typeorm migration:create -n CreateTransactions
+yarn typeorm migration:run
--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
@@ -9,18 +9,36 @@ const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
   // TODO
+  return response.json({ todo: 'you have things to do' });
 });
 
+// POST /transactions:
+// A rota deve receber title, value, type, e category dentro do corpo da requisição,
+// sendo o type o tipo da transação, que deve ser income para entradas (depósitos)
+// e outcome para saídas (retiradas).
+// Ao cadastrar uma nova transação, ela deve ser armazenada dentro do seu banco de dados,
+// possuindo os campos id, title, value, type, category_id, created_at, updated_at.
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, value, type, category } = request.body;
+  const createTransaction = new CreateTransactionService();
+  const transaction = await createTransaction.execute({
+    title,
+    value,
+    type,
+    category_title: category,
+  });
+
+  return response.status(201).json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
   // TODO
+  return response.json({ todo: 'you have things to do' });
 });
 
 transactionsRouter.post('/import', async (request, response) => {
   // TODO
+  return response.json({ todo: 'you have things to do' });
 });
 
 export default transactionsRouter;
